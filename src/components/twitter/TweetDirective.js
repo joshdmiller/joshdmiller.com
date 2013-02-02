4905135ccfbe65b9d802613ef113c1ca7941a91f
Josh David Miller <josh@joshdmiller.com>
2013-02-02 14:54:06 -0800

    feat(twitter): directive now adds 'a' links to handles and URLs

--- src/components/twitter/TweetDirective.js
+++ src/components/twitter/TweetDirective.js
@@ -1,4 +1,4 @@
-angular.module( 'twitter.directives', [] )
+angular.module( 'twitter.directives', [ 'ngSanitize' ] )
 
 .directive( 'tweet', [ function () {
   return {
@@ -6,24 +6,38 @@ angular.module( 'twitter.directives', [] )
     scope: true,
     //templateUrl: 'twitter/TweetDirective.tpl.html',
     template: '<span>'+
-        '<span class="tweet" ng-bind="body"></span> | '+
+        '<span class="tweet" ng-bind-html="body"></span> | '+
         '<span class="date">{{date | prettyDate}}</span> | '+
         '<a class="link-to-post" target="_blank" ng-href="https://twitter.com/joshdmiller/status/{{id}}">Visit</a>'+
       '</span>',
     link: function( scope, element, attrs ) {
+
+      function handlesToLinks( text ) {
+        return text.replace( 
+          /(@)([A-Za-z0-9_\-]+)/g, 
+          '<a target="_blank" href="http://twitter.com/$2">$&</a>'
+        );
+      }
+
+      function linksToAnchors( text ) {
+        return text.replace(
+          /(\b(https?|ftp):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig,
+          '<a target="_blank" href="$1">$1</a>'
+        );
+      }
+
       attrs.$observe( 'tweetBody', function ( val ) {
+        val = linksToAnchors( val );
+        val = handlesToLinks( val );
         scope.body = val;
-        console.log("body changed to", val);
       });
 
       attrs.$observe( 'tweet', function ( id ) {
         scope.id = id;
-        console.log("id changed to", id);
       });
 
       attrs.$observe( 'tweetDate', function ( date ) {
         scope.date = date;
-        console.log("date changed to", date);
       });
     }
   };
